c325fb1811c179aa46c026d203aa6d897825b666
RuandeSouza21 <105797473+RuandeSouza21@users.noreply.github.com>
2023-06-14 22:34:33 -0300

    Update scripts.js

--- Atividade/Projeto/scripts.js
+++ Atividade/Projeto/scripts.js
@@ -1,137 +1,206 @@
-const dictionary = ['custa', 'custo', 'terra', 'plano', 'muito', 'abala', 'areia'];
+var height = 6; //number of guesses
+var width = 5; //length of the word
 
+var row = 0; //current guess (attempt #)
+var col = 0; //current letter for that attempt
 
-const state = {
-    secret: dictionary[Math.floor(Math.random() * dictionary.length)],
-    grid: Array(6)
-          .fill()
-          .map(() => Array(5).fill('')),
-    currentRow: 0,
-    currentCol: 0,
-};
+var gameOver = false;
+// var word = "SQUID";
+var wordList = ['areia','abala']
 
-function updateGrid() {
-    for (let i = 0; i < state.grid.length; i++){
-        for (let j = 0; j < state.grid[i].length; j++){
-            const box = document.getElementById(`box${i}${j}`);
-            box.textContent = state.grid[i][j];
-        }
-    }
+var guessList = []
+
+guessList = guessList.concat(wordList);
+
+var word = wordList[Math.floor(Math.random()*wordList.length)].toUpperCase();
+console.log(word);
+
+window.onload = function(){
+    intialize();
 }
 
-function drawBox(container, row, col, letter = ''){
-    const box = document.createElement('div');
-    box.className = 'box';
-    box.id = `box${row}${col}`;
-    box.textContent = letter;
 
-    container.appendChild(box);
-    return box;
-}
+function intialize() {
 
-function drawGrid(container){
-    const grid = document.createElement('div');
-    grid.className = 'grid';
-
-    for (let i = 0; i < 6; i++){
-        for (let j = 0; j < 5; j++){
-            drawBox(grid, i, j);
+    // Create the game board
+    for (let r = 0; r < height; r++) {
+        for (let c = 0; c < width; c++) {
+            // <span id="0-0" class="tile">P</span>
+            let tile = document.createElement("span");
+            tile.id = r.toString() + "-" + c.toString();
+            tile.classList.add("tile");
+            tile.innerText = "";
+            document.getElementById("board").appendChild(tile);
         }
     }
 
-    container.appendChild(grid);
-}
+    // Create the key board
+    let keyboard = [
+        ["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P"],
+        ["A", "S", "D", "F", "G", "H", "J", "K", "L", " "],
+        ["Enter", "Z", "X", "C", "V", "B", "N", "M", "⌫" ]
+    ]
 
-function registerKeyboardEvents(){
-    document.body.onkeydown = (e) => {
-        const key = e.key;
-        if (key === 'Enter'){
-            if (state.currentCol === 5){
-                const word = getCurrentWord();
-                if (isWordValid(word)){
-                    revealWord(word);
-                    state.currentRow++;
-                    state.currentCol = 0;
-                } else {
-                    alert('A palavra não é válida');
-                }
+    for (let i = 0; i < keyboard.length; i++) {
+        let currRow = keyboard[i];
+        let keyboardRow = document.createElement("div");
+        keyboardRow.classList.add("keyboard-row");
+
+        for (let j = 0; j < currRow.length; j++) {
+            let keyTile = document.createElement("div");
+
+            let key = currRow[j];
+            keyTile.innerText = key;
+            if (key == "Enter") {
+                keyTile.id = "Enter";
             }
-        }
-        if (key === 'Backspace'){
-            removeLetter();
-        }
-        if (isLetter(key)){
-            addLetter(key);
-        }
+            else if (key == "⌫") {
+                keyTile.id = "Backspace";
+            }
+            else if ("A" <= key && key <= "Z") {
+                keyTile.id = "Key" + key; // "Key" + "A";
+            }
 
-        updateGrid();
-    };
-}
+            keyTile.addEventListener("click", processKey);
 
-function getCurrentWord(){
-    return state.grid[state.currentRow].reduce((prev, curr) => prev + curr);
-}
-
-function isWordValid(word){
-    return dictionary.includes(word);
-}
-
-function revealWord(guess){
-    const row = state.currentRow;
-    const animation_duration = 500;
-
-    for (let i = 0; i < 5; i++){
-        const box = document.getElementById(`box${row}${i}`);
-        const letter = box.textContent;
-
-        setTimeout(() => {
-            if (letter === state.secret[i]){
-                box.classList.add('right');
-            } else if (state.secret.includes(letter)){
-                box.classList.add('wrong');
+            if (key == "Enter") {
+                keyTile.classList.add("enter-key-tile");
             } else {
-                box.classList.add('empty');
+                keyTile.classList.add("key-tile");
             }
-        }, ((i + 1) * animation_duration) / 2);
-
-        box.classList.add('animated');
-        box.style.animationDelay = `${(i * animation_duration) / 2}ms`;
+            keyboardRow.appendChild(keyTile);
+        }
+        document.body.appendChild(keyboardRow);
     }
 
-    const isWinner = state.secret === guess;
-    const isGameOver = state.currentRow === 5;
 
-    setTimeout(() => {
-        if (isWinner) {
-            alert('Parabéns, você acertou!');
-        } else if (isGameOver){
-            alert(`Que pena você perdeu. A palavra era ${state.secret}.`);
+    // Listen for Key Press
+    document.addEventListener("keyup", (e) => {
+        processInput(e);
+    })
+}
+
+function processKey() {
+    e = { "code" : this.id };
+    processInput(e);
+}
+
+function processInput(e) {
+    if (gameOver) return;
+
+    // alert(e.code);
+    if ("KeyA" <= e.code && e.code <= "KeyZ") {
+        if (col < width) {
+            let currTile = document.getElementById(row.toString() + '-' + col.toString());
+            if (currTile.innerText == "") {
+                currTile.innerText = e.code[3];
+                col += 1;
+            }
         }
-    }, 3 * animation_duration);
+    }
+    else if (e.code == "Backspace") {
+        if (0 < col && col <= width) {
+            col -=1;
+        }
+        let currTile = document.getElementById(row.toString() + '-' + col.toString());
+        currTile.innerText = "";
+    }
+
+    else if (e.code == "Enter") {
+        update();
+    }
+
+    if (!gameOver && row == height) {
+        gameOver = true;
+        alert(`Que pena você perdeu. A palavra era ${word}.`);
+    }
 }
 
-function isLetter(key){
-    return key.length === 1 && key.match(/[a-z]/i);
+function update() {
+    let guess = "";
+    document.getElementById("answer").innerText = "";
+
+    //string up the guesses into the word
+    for (let c = 0; c < width; c++) {
+        let currTile = document.getElementById(row.toString() + '-' + c.toString());
+        let letter = currTile.innerText;
+        guess += letter;
+    }
+
+    guess = guess.toLowerCase(); //case sensitive
+    console.log(guess);
+
+    if (!guessList.includes(guess)) {
+        document.getElementById("answer").innerText = "Palavra não aceita";
+        return;
+    }
+
+    //start processing guess
+    let correct = 0;
+
+    let letterCount = {}; //keep track of letter frequency, ex) KENNY -> {K:1, E:1, N:2, Y: 1}
+    for (let i = 0; i < word.length; i++) {
+        let letter = word[i];
+
+        if (letterCount[letter]) {
+           letterCount[letter] += 1;
+        }
+        else {
+           letterCount[letter] = 1;
+        }
+    }
+
+    console.log(letterCount);
+
+    //first iteration, check all the correct ones first
+    for (let c = 0; c < width; c++) {
+        let currTile = document.getElementById(row.toString() + '-' + c.toString());
+        let letter = currTile.innerText;
+
+        //Is it in the correct position?
+        if (word[c] == letter) {
+            currTile.classList.add("correct");
+
+            let keyTile = document.getElementById("Key" + letter);
+            keyTile.classList.remove("present");
+            keyTile.classList.add("correct");
+
+            correct += 1;
+            letterCount[letter] -= 1; //deduct the letter count
+        }
+
+        if (correct == width) {
+            gameOver = true;
+            alert('Você ganhou!')
+        }
+    }
+
+    console.log(letterCount);
+    //go again and mark which ones are present but in wrong position
+    for (let c = 0; c < width; c++) {
+        let currTile = document.getElementById(row.toString() + '-' + c.toString());
+        let letter = currTile.innerText;
+
+        // skip the letter if it has been marked correct
+        if (!currTile.classList.contains("correct")) {
+            //Is it in the word?         //make sure we don't double count
+            if (word.includes(letter) && letterCount[letter] > 0) {
+                currTile.classList.add("present");
+
+                let keyTile = document.getElementById("Key" + letter);
+                if (!keyTile.classList.contains("correct")) {
+                    keyTile.classList.add("present");
+                }
+                letterCount[letter] -= 1;
+            } // Not in the word or (was in word but letters all used up to avoid overcount)
+            else {
+                currTile.classList.add("absent");
+                let keyTile = document.getElementById("Key" + letter);
+                keyTile.classList.add("absent")
+            }
+        }
+    }
+
+    row += 1; //start new row
+    col = 0; //start at 0 for new row
 }
-
-function addLetter(letter){
-    if (state.currentCol === 5) return;
-    state.grid[state.currentRow][state.currentCol] = letter;
-    state.currentCol++;
-}
-
-function removeLetter(){
-    if (state.currentCol === 0) return;
-    state.grid[state.currentRow][state.currentCol - 1] = '';
-    state.currentCol--;
-}
-
-function startup() {
-     const game = document.getElementById('game');
-     drawGrid(game);
-
-     registerKeyboardEvents();
-    console.log(state.secret);
-}
-
-startup();
